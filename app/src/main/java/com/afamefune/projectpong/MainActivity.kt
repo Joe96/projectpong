77bffdd0c63cc4dfa820e08606b15b1c5e8794fc
jafam <jafamefune@gmail.com>
2021-03-13 23:09:19 -0600

    tested listview by populating it with array from strings.xml.

--- app/src/main/java/com/afamefune/projectpong/MainActivity.kt
+++ app/src/main/java/com/afamefune/projectpong/MainActivity.kt
@@ -6,6 +6,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        // creates array list of month from array found in strings.xml
+        val language:Array<String> = resources.getStringArray(R.array.month_list)
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, language)
+
+        val listView = findViewById<ListView>(R.id.list_view)
+        listView.adapter = arrayAdapter
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
+            val selectedItem = adapterView.getItemAtPosition(position) as String
+            val itemIdAtPos = adapterView.getItemIdAtPosition(position)
+
+            Toast.makeText(applicationContext,"click item $selectedItem its position $itemIdAtPos",Toast.LENGTH_SHORT).show()
+        }
 
     }
 
